e02cbbbedda8f8dac4f463d98bd4191d0dea350f
Jeanel 'Nelly Nelle' Mouelle <jeanelmouelle@yahoo.com>
2015-10-01 08:47:09 -0400

    Question? How to make the console render true or false.

---
+++ main.js
@@ -0,0 +1,75 @@
+// Q1
+// Write a function called `sum` that takes two
+// parameters and returns the sum of those 2 numbers.
+
+function sum (numOne,numTwo) {
+	return numOne + numTwo
+}
+
+
+
+// Q2
+// Write a function named `avg` that takes 3 parameters
+// and returns the average of those 3 numbers.
+
+function avg (numOne,numTwo,numthree){
+	var total = (numOne + numTwo + numthree)
+	    return total/3;
+}
+
+
+
+// Q3
+// Write a function called `getLength` that takes one
+// parameter (a string) and returns the length
+
+var shoes = ["Nike","Toms","Prada"];
+console.log(shoes.getLength);
+
+var first = [0];
+var last = [shoes(shoes.getLength -1];
+
+shoes.forEach(function(item,index,array){
+	console.log(item,index);
+});
+
+
+
+// Q4
+// Write a function called `greaterThan` that takes
+// two parameters and returns `true` if the second
+// parameter is greater than the first. Otherwise
+// the function should return `false`.
+
+var numOne = 100;
+var numTwo = 50;
+
+document.Write (numOne >= numTwo);
+
+// How to render a true in console?
+
+// Q5
+// Write a function called `greet` that takes a
+// single parameter and returns a string that
+// is formated like "Hello, Name!" where *Name*
+// is the parameter that was passed in.
+
+
+
+
+// Q6
+// Write a function called `madlib` that takes
+// 4 or more parameters (words). The function 
+// should insert the words into a pre-defined
+// sentence. Finally the function should return
+// that sentence.
+// Note: When I say words and sentence I mean
+// strings. For example:
+// words: "quick", "fox", "fence"
+// sentence: "quick brown fox jumps over the fence"
+
+
+
+
+///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
